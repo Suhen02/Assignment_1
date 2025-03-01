f31a8a6b7c6c758c1143389f01c7d7392c9b654a
Suhen M <suhen.mg02@gmail.com>
2025-03-01 22:04:18 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,9 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+print(f"Addition of two number:{num1+num2}")
+print(f"subtration of two number:{num1-num2}")
+print(f"multiplication of two number:{num1*num2}")
+if num2==0:
+    print("division not possible!!!")
+else:
+    print(f"Division is :{num1/num2}")    
---
+++ task_2.py
@@ -0,0 +1,3 @@
+firstName=input("Enter the first name:")
+lastName=input("Enter the last name:")
+print(f"Hello,{firstName} {lastName}!welcome to the python program.")
